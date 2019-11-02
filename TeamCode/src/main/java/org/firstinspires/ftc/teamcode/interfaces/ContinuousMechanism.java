4edd9b92d106a992fae4ce75c330d275c4fd758d
Luke Early <23438839+LukeEarly@users.noreply.github.com>
2019-11-01 20:56:03 -0400

    add ContinuousMechanism.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/interfaces/ContinuousMechanism.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.interfaces;
+
+public interface ContinuousMechanism {
+    // set the value
+    public void setValue(double value);
+    // get the value
+    public double getValue();
+    // get the max
+    public double getMax();
+    // get the min
+    public double getMin();
+}
