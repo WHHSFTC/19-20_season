a12db0e1576200cad528bce6acb667c6bb41b0e6
Luke Early <23438839+LukeEarly@users.noreply.github.com>
2019-11-02 11:29:04 -0400

    add sursum drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/Robot.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.implementations;
+
+import org.firstinspires.ftc.teamcode.interfaces.StrafingDriveTrain;
+
+public class Robot {
+    public StrafingDriveTrain driveTrain = new SursumDriveTrain();
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/SursumDriveTrain.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.implementations;
+
+import org.firstinspires.ftc.teamcode.interfaces.StrafingDriveTrain;
+
+public class SursumDriveTrain implements StrafingDriveTrain {
+    @Override
+    public void goAngle(double dist, double angle, double power) {
+
+    }
+
+    @Override
+    public void goArc(double centerX, double centerY, double angle, double power) {
+
+    }
+
+    @Override
+    public void goVector(double x, double y, double power) {
+
+    }
+
+    @Override
+    public void rotate(double angle) {
+
+    }
+}
