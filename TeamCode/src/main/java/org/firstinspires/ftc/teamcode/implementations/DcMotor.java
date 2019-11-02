e173c6df58350b49e585d57b20be9e1860628d83
Luke Early <23438839+LukeEarly@users.noreply.github.com>
2019-11-02 11:00:03 -0400

    Add DcMotor stub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/DcMotor.java
@@ -0,0 +1,136 @@
+package org.firstinspires.ftc.teamcode.implementations;
+
+
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+
+public class DcMotor implements com.qualcomm.robotcore.hardware.DcMotor {
+    MotorConfigurationType motorType;
+    ZeroPowerBehavior zeroPowerBehavior;
+    boolean powerFloat;
+    int targetPosition;
+    double power;
+    Direction direction;
+    RunMode mode;
+
+    @Override
+    public MotorConfigurationType getMotorType() {
+        return motorType;
+    }
+
+    @Override
+    public void setMotorType(MotorConfigurationType motorType) {
+        this.motorType = motorType;
+    }
+
+    @Override
+    public DcMotorController getController() {
+        return null;
+    }
+
+    @Override
+    public int getPortNumber() {
+        return 0;
+    }
+
+    @Override
+    public void setZeroPowerBehavior(ZeroPowerBehavior zeroPowerBehavior) {
+        this.zeroPowerBehavior = zeroPowerBehavior;
+    }
+
+    @Override
+    public ZeroPowerBehavior getZeroPowerBehavior() {
+        return zeroPowerBehavior;
+    }
+
+    @Override
+    public void setPowerFloat() {
+        powerFloat = true;
+    }
+
+    @Override
+    public boolean getPowerFloat() {
+        return powerFloat;
+    }
+
+    @Override
+    public void setTargetPosition(int position) {
+        targetPosition = position;
+    }
+
+    @Override
+    public int getTargetPosition() {
+        return targetPosition;
+    }
+
+    @Override
+    public boolean isBusy() {
+        return false;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void setMode(RunMode mode) {
+        this.mode = mode;
+    }
+
+    @Override
+    public RunMode getMode() {
+        return mode;
+    }
+
+    @Override
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public Direction getDirection() {
+        return direction;
+    }
+
+    @Override
+    public void setPower(double power) {
+        this.power = power;
+
+    }
+
+    @Override
+    public double getPower() {
+        return power;
+    }
+
+    @Override
+    public Manufacturer getManufacturer() {
+        return null;
+    }
+
+    @Override
+    public String getDeviceName() {
+        return "Stub";
+    }
+
+    @Override
+    public String getConnectionInfo() {
+        return null;
+    }
+
+    @Override
+    public int getVersion() {
+        return 0;
+    }
+
+    @Override
+    public void resetDeviceConfigurationForOpMode() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
