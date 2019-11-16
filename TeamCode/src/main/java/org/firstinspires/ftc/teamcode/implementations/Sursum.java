a37a44ccb5c26fdab540d560f6203b60de2f485f
Khadija <kcoulibaly360@gmail.com>
2019-11-16 12:49:20 -0500

    Added distance sensor test and made a mock 1-stone auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/Sursum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/Sursum.java
@@ -42,7 +42,7 @@ public class Sursum {
         // }}}
 
         // sensors {{{
-        // ods = opMode.hardwareMap.get(DistanceSensor.class, "ods");
+        ods = opMode.hardwareMap.get(DistanceSensor.class, "ods");
         // }}}
     }
     public void init() {
