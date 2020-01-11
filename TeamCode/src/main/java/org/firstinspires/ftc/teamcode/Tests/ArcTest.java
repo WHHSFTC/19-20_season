d1fd3cecee353a2e0dce7721d82b0fdd845136e2
Luke Early <23438839+LukeEarly@users.noreply.github.com>
2020-01-11 16:23:02 -0500

    foundarc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ArcTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ArcTest.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.Tests;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.implementations.DriveTrain;
+import org.firstinspires.ftc.teamcode.implementations.ShuttleGate;
 import org.firstinspires.ftc.teamcode.implementations.Sursum;
 import org.firstinspires.ftc.teamcode.interfaces.OpModeIF;
 
@@ -22,8 +23,17 @@ public class ArcTest extends LinearOpMode implements OpModeIF {
         bot.driveTrain.setHeading(DriveTrain.BLUE_SIDE);
         waitForStart();
         bot.driveTrain.setZeroPowerBehaviors(DcMotor.ZeroPowerBehavior.BRAKE);
-        bot.driveTrain.goArc(10,90,90,.5);
+        bot.shuttleGate.setState(ShuttleGate.State.FOUNDATION);
+        bot.driveTrain.goArc(ArcConstants.distance, ArcConstants.frontAngle, ArcConstants.turnAngle, ArcConstants.power);
         bot.stop();
+    }
+
+    @Config
+    public static class ArcConstants {
+        public static double distance = 14;
+        public static double frontAngle = 90;
+        public static double turnAngle = 90;
+        public static double power = .5;
     }
 
     @Override
