c404f5cd59f106a6cd1be5ab4a51b917572b49e9
JarnaChao09 <yunyang4928@gmail.com>
2020-02-18 16:02:39 -0500

    starting new bot changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/OutputSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/OutputSlides.java
@@ -9,36 +9,23 @@ import static java.lang.Thread.sleep;
 
 // drives slide motors using sensors and encoders
 public class OutputSlides implements ContinuousMechanism {
-    private DcMotor motor1;
-    private DcMotor motor2;
-    private DcMotor motor3;
+    private DcMotor motor;
     private OpModeIF opMode;
-    public OutputSlides(OpModeIF opMode, String str1, String str2, String str3) {
-        motor1 = opMode.getHardwareMap().dcMotor.get(str1);
-        motor2 = opMode.getHardwareMap().dcMotor.get(str2);
-        motor3 = opMode.getHardwareMap().dcMotor.get(str3);
-        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motor3.setDirection(DcMotorSimple.Direction.REVERSE);
-        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    public OutputSlides(OpModeIF opMode, String str) {
+        motor = opMode.getHardwareMap().dcMotor.get(str);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         this.opMode = opMode;
     }
     @Override
     public void setState(Double value) throws IllegalArgumentException {
-        motor1.setPower(value);
-        motor2.setPower(value);
-        motor3.setPower(value);
+        motor.setPower(value);
     }
 
     @Override
     public Double getState() {
-        return motor1.getPower();
+        return motor.getPower();
     }
 
     @Override
@@ -52,34 +39,21 @@ public class OutputSlides implements ContinuousMechanism {
     }
 
     public void dumpEncoders() {
-        opMode.getTelemetry().addData("spool 1", motor1.getCurrentPosition());
-        opMode.getTelemetry().addData("spool 2", motor2.getCurrentPosition());
-        opMode.getTelemetry().addData("spool 3", motor3.getCurrentPosition());
+        opMode.getTelemetry().addData("spool", motor.getCurrentPosition());
     }
     public void stop() {
         setState(0.0);
     }
 
     public void runToZero() {
-        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor1.setTargetPosition(0);
-        motor2.setTargetPosition(0);
-        motor3.setTargetPosition(0);
-        motor1.setPower(0.5);
-        motor2.setPower(0.5);
-        motor3.setPower(0.5);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setTargetPosition(0);
+        motor.setPower(0.5);
     }
 
     public void runToPosition(int position, double power) throws InterruptedException {
-        int threshold = 100;
-        motor1.setTargetPosition(-position);
-        motor2.setTargetPosition(-position);
-        motor3.setTargetPosition(-position);
-        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setTargetPosition(-position);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         setState(power);
         Thread.sleep(500);
         setState(0.0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/Sursum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/implementations/Sursum.java
@@ -1,20 +1,14 @@
 package org.firstinspires.ftc.teamcode.implementations;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.interfaces.ContinuousMechanism;
 import org.firstinspires.ftc.teamcode.interfaces.Mechanism;
 import org.firstinspires.ftc.teamcode.interfaces.OpModeIF;
 import org.firstinspires.ftc.teamcode.interfaces.StrafingDriveTrain;
-import org.firstinspires.ftc.teamcode.tests.VuforiaStuff;
 
 /**
  * Bot Class
@@ -38,25 +32,16 @@ public class Sursum {
     public ContinuousMechanism outputSlides;
     public Mechanism<Arm.State> arm;
     public Mechanism<Claw.State> claw;
-    public CRServo flywheels;
+    public DcMotorSimple flywheels;
     public DcMotor belt;
     public LeftSideArm leftArm;
     public RightSideArm rightArm;
     public SideArm sideArm;
-
-//    public DistanceSensor ods;
-//    public DigitalChannel limit;
-//    public ColorSensor color_sensor_bottom;
-//    public Vision vision;
-//    public VuforiaStuff vuforiaStuff;
-//    public VuforiaLocalizer vuforia;
-//    VuforiaStuff.skystonePos pos;
-//    public static final String VUFORIA_KEY = "AZjnTyD/////AAABmWbY5Kf/tUDGlNmyg0to/Ocsr2x5NKR0bN0q9InlH4shr90xC/iovUPDBu+PWzwD2+F8moAWhCpUivQDuKp/j2IHVtyjoKOQvPkTaXAb1IgPtAM6pMDltXDTkQ8Olwds22Z97Wdx+RAPK8WrC809Hj+JDZJJ3/Lx3bqAwcR1TRJ4OejxkWVSAKvFX8rOp5gE82jPNEv1bQ5S+iTgFtToZNQTj2ldtYJjoSkyUHqfODyV3JUazYSu82UEak0My2Ks/zIXYrDEY0y5MgNzRr9pzg3AiA8pbUT3SVk3SSUYmjlml+H9HovgDuiGrnJnmNMSjQGfcGpliGW6fs61ePYuAHvN4+Rwa1esR/prFgYKrTTn";
-
     public VisionTF visionTF;
     public OpModeIF opMode;
-    public DigitalChannel allianceSwitch;
     public Alliance alliance;
+
+    private DigitalChannel allianceSwitch;
 
     /**
      * Creation of all systems of the bot
@@ -65,65 +50,41 @@ public class Sursum {
     public Sursum(OpModeIF opMode) {
         this.opMode = opMode;
         driveTrain = new DriveTrain(opMode, "motorRF", "motorLF", "motorLB", "motorRB");
-        // ((DriveTrain) driveTrain).stubify();
         shuttleGate = new ShuttleGate(opMode, "leftGate", "rightGate");
 
-
         // output {{{
-        outputSlides = new OutputSlides(opMode, "spool1", "spool2", "spool3");
+        outputSlides = new OutputSlides(opMode, "spool");
         arm = new Arm(opMode, "elbow", "wrist");
         claw = new Claw(opMode, "inner", "outer");
         // }}}
 
         // intake {{{
-        flywheels = new Flywheels(opMode, "leftFly", "rightFly");
+        flywheels = new FlyWheels(opMode, "leftIn", "rightIn");
         belt = opMode.getHardwareMap().dcMotor.get("belt");
         leftArm = new LeftSideArm(opMode, "leftArm", "leftClaw");
         rightArm = new RightSideArm(opMode, "rightArm", "rightClaw");
-//        rightArm.arm.setDirection(Servo.Direction.REVERSE);
-//        rightArm.claw.setDirection(Servo.Direction.REVERSE);
-        // }}}
-
-        // sensors {{{
-//        ods = opMode.hardwareMap.get(DistanceSensor.class, "ods");
-//        limit = opMode.hardwareMap.digitalChannel.get("limit");
-//        color_sensor_bottom = opMode.getHardwareMap().colorSensor.get("color");
         allianceSwitch = opMode.getHardwareMap().digitalChannel.get("allianceSwitch");
         allianceSwitch.setMode(DigitalChannel.Mode.INPUT);
-    //    vuforiaStuff = new VuforiaStuff(vuforia);
         // }}}
-
-//        vision = new Vision(opMode, "Webcam 1");
     }
 
     public void init() {
-        //init(allianceSwitch.getVoltage() / allianceSwitch.getMaxVoltage() > 0.5 ? Alliance.RED : Alliance.BLUE);
         init(allianceSwitch.getState() ? Alliance.BLUE : Alliance.RED);
         opMode.getTelemetry().addData("Alliance", alliance.toString());
-        //init(Alliance.RED);
     }
 
     public void init(Alliance alliance) {
         this.alliance = alliance;
-      //  int cameraMonitorViewId = opMode.getHardwareMap().appContext.getResources().getIdentifier("cameraMonitorViewId", "id", opMode.getHardwareMap().appContext.getPackageName());
-        //VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        //parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        //parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        //vuforia = ClassFactory.getInstance().createVuforia(parameters);
         switch(this.alliance) {
             case RED:
                 our_side = DriveTrain.RED_SIDE;
                 opponents_side = DriveTrain.BLUE_SIDE;
                 sideArm = rightArm;
-          //      pos = vuforiaStuff.vuforiascan(false,true);
                 break;
             case BLUE:
                 our_side = DriveTrain.BLUE_SIDE;
                 opponents_side = DriveTrain.RED_SIDE;
                 sideArm = leftArm;
-            //    pos = vuforiaStuff.vuforiascan(false,false);
-
         }
         // reset the motors
         driveTrain.setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -136,22 +97,6 @@ public class Sursum {
         rightArm.arm.setState(SideArm.Arm.State.UP);
         leftArm.claw.setState(SideArm.Claw.State.CLOSED);
         rightArm.claw.setState(SideArm.Claw.State.CLOSED);
-        // start vision
-        /*switch (pos){
-            case RIGHT:
-                opMode.getTelemetry().addLine("Skystone Pos: RIGHT");
-                opMode.getTelemetry().update();
-                break;
-            case CENTER:
-                opMode.getTelemetry().addLine("Skystone Pos: CENTER");
-                opMode.getTelemetry().update();
-                break;
-            case LEFT:
-                opMode.getTelemetry().addLine("Skystone Pos: LEFT");
-                opMode.getTelemetry().update();
-                break;
-        }
-*/
 
         // set zero power behaviors to float so Kaden can turn the bot
         driveTrain.setZeroPowerBehaviors(DcMotor.ZeroPowerBehavior.FLOAT);
@@ -165,10 +110,6 @@ public class Sursum {
      */
     public SkyStonePosition findSkystone() throws InterruptedException {
         for(SkyStonePosition position : new SkyStonePosition[] {SkyStonePosition.THREE_SIX, SkyStonePosition.TWO_FIVE}) {
-//            Thread.sleep(500);
-////            opMode.telemetry.addData("Tensorflow Object", object);
-//            opMode.telemetry.addData("Confidence: ", "N/A");
-//            opMode.telemetry.update();
             if (visionTF.getStone()) {
                 driveTrain.goAngle(3.25, DriveTrain.LOADING_ZONE, .25);
                 return position;
@@ -186,9 +127,6 @@ public class Sursum {
     public void intake() throws InterruptedException {
         flywheels.setPower(-2.0/3);
         belt.setPower(-1);
-      //while (!limit.getState()) {
-      //    Thread.sleep(100);
-      //}
         Thread.sleep(500);
         flywheels.setPower(0);
         Thread.sleep(500);
